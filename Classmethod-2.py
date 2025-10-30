869d01702edf18a816a3fb21534065936e199879
Harshad0804 <harshadkarguwal@gmail.com>
2025-10-30 19:49:15 +0530

    OOPS-python

---
+++ Classmethod-2.py
@@ -0,0 +1,13 @@
+class Pizza:
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    @classmethod
+    def margherita(cls):
+        return cls(["mozzarella", "corn"])
+
+    def __str__(self):
+        return f"Pizza with {' and '.join(self.ingredients)}"
+
+pizza1 = Pizza.margherita()
+print(pizza1)
